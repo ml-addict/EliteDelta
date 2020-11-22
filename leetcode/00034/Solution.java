ce6dea2de95fec3ff16ec24679aa5dd9192dbee9
ml-addict <brian.jie.lin@gmail.com>
2020-11-22 01:55:23 +0000

    Tested 34. Find First and Last Position of Element in Sorted Array.

--- leetcode/00034/Solution.java
+++ leetcode/00034/Solution.java
@@ -6,35 +6,61 @@ public class Solution {
 
         int mid;
 
-        while (left < right) {
-            mid = left + (right / 2);
+        while (left <= right) {
+            mid = left + (right-left) / 2;
             if (nums[mid] == target) {
                 int leftEnd = findLeft(nums, left, mid-1, target);
                 int rightEnd = findRight(nums, mid+1, right, target);
                 return new int[]{leftEnd, rightEnd};
             } else if (nums[mid] > target) {
-                right = mid;
+                right = mid - 1;
             } else {
-                left = mid;
+                left = mid + 1;
             }
         }
         // Can't find the target.
         return new int[]{-1, -1};
     }
 
+    /**
+     * Find the left range index of the target.
+     * [1, 2, 3, 4] target = 4, left = 0, right = 3
+     * mid = 1, nums[mid] = 2
+     * 2 != 4, left = 1
+     * ---
+     * mid = 2, nums[mid] = 3
+     * left = 3
+     * ---
+     * mid = 4
+     */
     public int findLeft(int[] nums, int left, int right, int target) {
-        while (left < right) {
-            mid = left + (right / 2);
+        while (left <= right) {
+            int mid = left + (right-left) / 2;
             if (nums[mid] == target) {
                 right = mid-1;
             } else {
                 left = mid + 1;
             }
         }
-        return 0;
+        return left;
     }
 
     public int findRight(int[] nums, int left, int right, int target) {
-        return 0;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return right;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {};
+        int[] result = s.searchRange(nums, 0);
+        System.out.println(result[0] + "," + result[1]);
     }
 }
